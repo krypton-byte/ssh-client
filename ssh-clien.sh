e47acf115d463b5e05d2e2d61d06df11486e373c
puja04122003 <52121207+puja04122003@users.noreply.github.com>
2019-06-26 07:43:23 +0000

    Add files via upload

---
+++ ssh-clien.sh
@@ -0,0 +1,27 @@
+
+
+if [ -f $PREFIX/bin/ssh ]
+then
+clear
+Gar=═;R='\033[1;31m';G='\033[1;32m';xz=echo;Sd=0.1;B='\033[1;34m';xxx=sleep;cpu=ssh;vcp=fig;go=let;Y='\033[1;33m';op=masukan;opt=-n;poi=lol;C='\033[1;36m';Cc="author";A="puja";kucing=cat;D='\033[0m'
+kum=║
+${xxx} ${Sd} ;${xz} "     ╔${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}╗"|${poi}${kucing}
+${xz} "     ${kum} ___ ___| |__         ___| (_) ___ _ __ | |_    ${kum}"|${poi}${kucing}
+${xz} "     ${kum}/ __/ __| '_ \ _____ / __| | |/ _ \ '_ \| __|   ${kum}"|${poi}${kucing}
+${xz} "     ${kum}\__ \__ \ | | |_____| (__| | |  __/ | | | |_    ${kum}"|${poi}${kucing}
+${xz} "     ${kum}|___/___/_| |_|      \___|_|_|\___|_| |_|\__|   ${kum}"|${poi}${kucing}
+${xxx} ${Sd} ;${xz} "     ╚${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}${Gar}╝"|${poi}${kucing}
+${xxx} ${Sd} ;${xz} "      ${G}${Cc}:${A}"
+${xxx} ${Sd} ;${xz} "      ${G}http://github.com/puja04122003"
+${xxx} ${Sd} ;${xz} ${opt} "${Y}[${G}?${Y}]${R}${op} username${C}=>${Y}";read user;
+${xxx} ${Sd} ;${xz} ${opt} "${Y}[${G}?${Y}]${R}${op} alamat ip${C}=>${Y}";read ip;
+${xxx} ${Sd} ;${xz} ${opt} "${Y}[${G}?${Y}]${R}${op} port${C}=>${Y}";read port;
+${xxx} ${Sd} ;${xz} "$D"
+${xxx} ${Sd} ;${xz} "${R}[${Y}+${R}]${G} harap tunggu${D} "
+${xxx} ${Sd} ;${xz} ${opt} "$G<";${xxx} ${Sd} ;${xz} ${opt} "<";${xxx} ${Sd} ;${xz} ${opt} "<";${xxx} ${Sd} ;${xz} ${opt} "<";${xxx} ${Sd} ;${xz} ${opt} "<";${xxx} ${Sd};${xz} ${opt} "<";${xxx} ${Sd} ;${xz} ${opt} "<";${xxx} ${Sd};${xz} ${opt} "<";${xz} ${opt} "<";${xxx} ${Sd} ;${xz} ${opt} "<$B";${xxx} ${Sd} ;${xz} ${opt} "<";${xxx} ${Sd} ;${xz} ${opt} "<";${xxx} ${Sd} ;${xz} ${opt} "<";${xxx} ${Sd};${xz} ${opt} "<";${xxx} ${Sd} ;${xz} ${opt} "<";${xxx} ${Sd};${xz} ${opt} "<";${xxx} ${Sd};${xz} ${opt} "<";${xxx} ${Sd} ;${xz} ${opt} "<";${xxx} ${Sd} ;${xz} ${opt} "<";${xxx} ${Sd} ;${xz} ${opt} "<$R";${xxx} ${Sd} ;${xz} ${opt} "<";${xxx} ${Sd} ;${xz} ${opt} "<";${xxx} ${Sd} ;${xz} ${opt} "<";${xxx} ${Sd} ;${xz} ${opt} "<";${xxx} ${Sd} ;${xz} ${opt} "<";${xxx} ${Sd} ;${xz} ${opt} "<";${xxx} ${Sd} ;${xz} ${opt} "<";${xxx} ${Sd} ;${xz} ${opt} "<";${xxx} ${Sd};${xz} ${opt} "$G+$R";${xxx} ${Sd} ;${xz} ${opt} ">";${xxx} ${Sd} ;${xz} ${opt} ">";${xxx} ${Sd} ;${xz} ${opt} ">";${xxx} ${Sd} ;${xz} ${opt} ">";${xxx} ${Sd} ;${xz} ${opt} ">";${xxx} ${Sd} ;${xz} ${opt} ">";${xxx} ${Sd} ;${xz} ${opt} ">";${xxx} ${Sd} ;${xz} ${opt} ">";${xxx} ${Sd} ;${xz} ${opt} ">$B";${xxx} ${Sd} ;${xz} ${opt} ">";${xxx} ${Sd} ;${xz} ${opt} ">";${xxx} ${Sd} ;${xz} ${opt} ">";${xxx} ${Sd} ;${xz} ${opt} ">";${xxx} ${Sd} ;${xz} ${opt} ">";${xxx} ${Sd} ;${xz} ${opt} ">";${xxx} ${Sd} ;${xz} ${opt} ">";${xz} ${opt} ">";${xxx} ${Sd} ;${xz} ${opt} ">";${xxx} ${Sd} ;${xz} ${opt} ">$G";${xxx} ${Sd} ;${xz} ${opt} ">";${xxx} ${Sd} ;${xz} ${opt} ">";${xxx} ${Sd} ;${xz} ${opt} ">";${xxx} ${Sd} ;${xz} ${opt} ">";${xxx} ${Sd} ;${xz} ${opt} ">";${xz} ${opt} ">";${xxx} ${Sd} ;${xz} ${opt} ">";${xxx} ${Sd} ;${xz} ${opt} ">";${xxx} ${Sd} ;${xz} ${opt} ">";${xxx} ${Sd} ;${xz} ${opt} ">$D"
+${xz} ""
+${xxx} 1
+${cpu} -v ${user}@${ip} -p ${port}
+else
+pkg install openssh
+fi
